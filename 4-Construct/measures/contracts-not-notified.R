6406de7827505b0e58e3d9a04b4d8d456c5b7bb6
Shaun McGirr <shaunmcgirr@gmail.com>
2016-06-21 16:26:55 +1200

    Adding inventory of measures spreadsheet and a subdirectory to store the scripts

---
+++ 4-Construct/measures/contracts-not-notified.R
@@ -0,0 +1,20 @@
+# 4-Construct\measures\contracts-not-notified.R
+
+###################
+# 1. Prepare data #
+###################
+
+
+########################
+# 2. Calculate measure #
+########################
+
+
+#####################
+# 3. Store measures #
+#####################
+
+
+
+
+# ENDS
